35cf16e52e9dfc5ba4ac1eb084332bb9bba1e90f
tekemehmet <tekemehmet@yahoo.com>
2020-09-25 21:52:06 -0400

    first commit

---
+++ src/main/java/com/guru99/pages/BasePage.java
@@ -0,0 +1,18 @@
+package com.guru99.pages;
+
+import commonLibs.implementation.ElementControl;
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    WebDriver driver;
+    public ElementControl elementControl;
+
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+
+        elementControl = new ElementControl(driver);
+    }
+
+
+}
---
+++ src/main/java/commonLibs/implementation/CommonDriver.java
@@ -0,0 +1,65 @@
+package commonLibs.implementation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class CommonDriver {
+
+    private WebDriver driver;
+
+    private int pageLoadTimeout;
+
+    private int elementDetectionTimeout;
+
+    private String currentWorkingDirectory;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setPageLoadTimeout(int pageLoadTimeout) {
+        this.pageLoadTimeout = pageLoadTimeout;
+    }
+
+    public void setElementDetectionTimeout(int elementDetectionTimeout) {
+        this.elementDetectionTimeout = elementDetectionTimeout;
+    }
+
+    public CommonDriver(String browserType) throws Exception {
+
+        pageLoadTimeout = 10;
+
+        elementDetectionTimeout = 10;
+
+        currentWorkingDirectory = System.getProperty("user.dir");
+
+        if(browserType.equalsIgnoreCase("chrome")){
+
+            System.setProperty("webdriver.chrome.driver",currentWorkingDirectory+"/drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        }else if(browserType.equalsIgnoreCase("edge")){
+
+            System.setProperty("webdriver.edge.driver",currentWorkingDirectory+"/drivers/msedgedriver.exe");
+            driver = new EdgeDriver();
+
+        }else{
+            throw new Exception("Invalid Browser Type"+ browserType);
+        }
+
+        driver.manage().window().maximize();
+
+        driver.manage().deleteAllCookies();
+
+    }
+
+    public void navigateToUrl(String url){
+
+        driver.manage().timeouts().pageLoadTimeout(pageLoadTimeout, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(elementDetectionTimeout, TimeUnit.SECONDS);
+
+        driver.get(url);
+    }
+}
